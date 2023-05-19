077029e61a3103581391ab34531ef3877a9747aa
Annie <aneliya.boneva@gmail.com>
2023-05-19 15:37:40 +0300

    Adding to BoardR-3 homework Task class (cannot implement the EventLog to work properly) Issue class - finished

--- Aneliya's homeworks/BoardR-in-class activity/src/BoardItem.java
+++ Aneliya's homeworks/BoardR-in-class activity/src/BoardItem.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class BoardItem {
+public abstract class BoardItem {
 
     private static final int MAX_TITLE_LENGTH = 30;
     private static final int MIN_TITLE_LENGTH = 5;
@@ -14,7 +14,7 @@ public class BoardItem {
     private String title;
     private LocalDate dueDate;
     private Status status;
-    private final List<EventLog> history;
+    public final List<EventLog> history; //private to public so that Task can use it as well
 
 
     public BoardItem(String title, LocalDate dueDate) {
@@ -22,16 +22,20 @@ public class BoardItem {
         this.title = title;
         validateDueDate(dueDate);
         this.dueDate = dueDate;
-        //setTitle(title);
-        //setDueDate(dueDate);
         this.status = Status.OPEN;
         history = new ArrayList<>();
-        history.add(new EventLog(String.format("Item created: %s", viewInfo())));
+        history.add(new EventLog(String.format(
+                "Item created: %s", viewInfo())));
     }
 
     public List<EventLog> getHistory() {
         return new ArrayList<>(history);
     }
+
+//    public void setHistory(){
+//        history.add(new EventLog(String.format(
+//                "Item created: %s", viewInfo())));
+//    }
 
     public String getTitle() {
         return title;
@@ -39,9 +43,9 @@ public class BoardItem {
 
     public void setTitle(String title) {
         validateTitle(title);
-        history.add(new EventLog(String.format("Title changed from %s to %s",getTitle(), title)));
+        history.add(new EventLog(String.format(
+                "Title changed from %s to %s", getTitle(), title)));
         this.title = title;
-
     }
 
     public LocalDate getDueDate() {
@@ -50,9 +54,9 @@ public class BoardItem {
 
     public void setDueDate(LocalDate dueDate) {
         validateDueDate(dueDate);
-        history.add(new EventLog(String.format("DueDate changed from %s to %s", getDueDate(), dueDate)));
+        history.add(new EventLog(String.format(
+                "DueDate changed from %s to %s", getDueDate(), dueDate)));
         this.dueDate = dueDate;
-
     }
 
     public String getStatus() {
@@ -60,39 +64,37 @@ public class BoardItem {
     }
 
     public void setStatus(Status status) {
-        history.add(new EventLog(String.format("Status changed from %s to %s", getStatus(), status)));
+        history.add(new EventLog(String.format(
+                "Status changed from %s to %s", getStatus(), status)));
         this.status = status;
     }
 
-    public void revertStatus(){
-        if(status != Status.OPEN) {
-            setStatus(Status.values()[status.ordinal()-1]);
+    public void revertStatus() {
+        if (status != Status.OPEN) {
+            setStatus(Status.values()[status.ordinal() - 1]);
         } else {
             history.add(new EventLog(String.format(
                     "Can't revert, already at %s", Status.values()[0])));
         }
-
-
     }
 
     public void advanceStatus() {
-        if(status != Status.VERIFIED) {
-            setStatus(Status.values()[status.ordinal()+1]);
+        if (status != Status.VERIFIED) {
+            setStatus(Status.values()[status.ordinal() + 1]);
         } else {
             history.add(new EventLog(String.format(
-                    "Can't advance, already at %s", Status.values()[Status.values().length-1])));
+                    "Can't advance, already at %s", Status.values()[Status.values().length - 1])));
         }
     }
 
     public String viewInfo() {
         //return String.format("%s, [%s | %tY-%tm-%td ]", title, status, dueDate, dueDate, dueDate);
         return String.format("%s, [%s | %s ]", title, status, dueDate);
-
     }
 
 
     public void displayHistory() {
-        for (EventLog event: getHistory()) {
+        for (EventLog event : getHistory()) {
             System.out.println(event.viewInfo());
         }
     }
@@ -104,10 +106,10 @@ public class BoardItem {
     }
 
     private static void validateDueDate(LocalDate dueDate) {
-        if (dueDate.isBefore(LocalDate.now())){
+        if (dueDate.isBefore(LocalDate.now())) {
             throw new IllegalArgumentException(ERROR_DUE_DATE);
         }
     }
-
-
 }
+
+
--- Aneliya's homeworks/BoardR-in-class activity/src/Main.java
+++ Aneliya's homeworks/BoardR-in-class activity/src/Main.java
@@ -4,25 +4,23 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
 
+//        Issue issue = new Issue(
+//                "App flow tests?",
+//                "We need to test the App!",
+//                LocalDate.now().plusDays(1));
+//        issue.advanceStatus();
+//        issue.setDueDate(issue.getDueDate().plusDays(1));
+//        issue.displayHistory();
 
-        BoardItem item = new BoardItem("Refactor this mess", LocalDate.now().plusDays(2));
-       item.setDueDate(item.getDueDate().plusYears(2));
-        item.setTitle("Not that important");
-        item.revertStatus();
-        item.advanceStatus();
-        item.revertStatus();
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
+        Issue issue = new Issue("App flow tests?", "We need to test the App!", tomorrow);
+        Task task = new Task("Test the application flow", "Pesho", tomorrow);
 
-        item.displayHistory();
+        Board board = new Board();
 
-        System.out.println("\n--------------\n");
-
-        BoardItem anotherItem = new BoardItem("Don't refactor anything",  LocalDate.now().plusDays(10));
-        anotherItem.advanceStatus();
-        anotherItem.advanceStatus();
-        anotherItem.advanceStatus();
-        anotherItem.advanceStatus();
-        anotherItem.advanceStatus();
-        anotherItem.displayHistory();
+        board.addItem(issue);
+        board.addItem(task);
+        System.out.println(board.totalItems()); // 2
 
     }
 }
